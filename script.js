f2d2e71a7b4689da60fd5ff32bb97f17c7c334b5
MatTeo-BT <aboutmet@gmail.com>
2023-11-09 17:35:59 +0100

    inserimento prompt

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let firstName="Matteo";
+
+let firstName=prompt(`Dimmi il tuo nome`);
 console.log(firstName);
-let surname="Battisti";
+
+let surname=prompt(`Dimmi il tuo cognome`);
 console.log(surname);
-let favouriteColor="Verde";
+
+let favouriteColor=prompt(`Dimmi il tuo colore preferito`);
 console.log(favouriteColor);
+let sum= firstName + surname + favouriteColor;
+
+document.getElementById(`dati-utente`).innerHTML = `La password è :`
